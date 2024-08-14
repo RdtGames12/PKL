18fd54420da9cbf70e81ec4045f734a019e35a65
FikriBagja <fikribr10@gmail.com>
2024-08-14 10:36:52 +0700

    update

---
+++ Frontend/src/App.vue
@@ -0,0 +1,79 @@
+<template>
+  <div>
+    <search @update-query="fetchFilms"></search>
+    <div class="container">
+    <div v-if="movies.length" class="d-flex flex-wrap justify-content-between">
+      <div
+        v-for="movie in movies"
+        :key="movie.imdbID"
+        class="col-md-4 mb-4 px-2"
+      >
+        <div class="card shadow-sm">
+          <div class="card-header">
+            <img
+              class="card-img-top"
+              :src="movie.Poster"
+              alt="Card image cap"
+            />
+          </div>
+          <div class="card-body">
+            <h5 class="card-title">{{ movie.Title }}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">
+              Year: {{ movie.Year }}
+            </h6>
+            <a
+              href=""
+              class="card-link see-detail"
+              data-bs-toggle="modal"
+              data-bs-target="#exampleModal"
+              :data-id="movie.imdbID"
+              >See Detail Movies</a
+            >
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div v-else>
+      <p>No results found</p>
+    </div>
+  </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import search from "./components/search.vue";
+
+export default {
+  components: {
+    search,
+  },
+  data() {
+    return {
+      movies: [],
+    };
+  },
+  methods: {
+    fetchFilms(query) {
+      if (query) {
+        axios
+          .get(`http://www.omdbapi.com?apikey=184a9527&s=${query}`)
+          .then((response) => {
+            if (response.data.Search) {
+              this.movies = response.data.Search;
+            } else {
+              this.movies = [];
+            }
+          })
+          .catch((error) => {
+            console.error(error);
+            this.movies = [];
+          });
+      } else {
+        this.movies = [];
+      }
+    },
+  },
+};
+</script>
---
+++ Frontend/src/components/search.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="mb-4">
+      <div class="input-group d-flex justify-content-center">
+        <button class="btn btn-secondary" type="button" aria-readonly="true">
+          Search
+        </button>
+        <input 
+          type="text" 
+          class="form-control-sm " 
+          placeholder="Search Movies..." 
+          aria-label="Search Movies" 
+          name="search" 
+          v-model="searchQuery" 
+          @input="emitSearchQuery"
+        />
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        searchQuery: ''
+      }
+    },
+    methods: {
+      emitSearchQuery() {
+        this.$emit('update-query', this.searchQuery);
+      }
+    }
+  }
+  </script>
+  

d7a47802a744679ef6764fd530315c83b8d2295c
FikriBagja <fikribr10@gmail.com>
2024-08-21 13:17:56 +0700

    ubah detail

--- Frontend/FiturData/src/views/Kios.vue
+++ Frontend/FiturData/src/views/Kios.vue
@@ -93,7 +93,7 @@ onMounted(() => {
                     'text-red-600': !detail.isActive,
                   }"
                 >
-                  {{ detail.isActive }}
+                  {{ detail.isActive ? 'ON' : 'OFF' }}
                 </span>
               </div>
             </div>

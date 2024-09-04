ee74707d247a0427a1a7a96f87649d4e9cd79438
FikriBagja <fikribr10@gmail.com>
2024-09-04 13:24:04 +0700

    jadi

--- Frontend/FiturDataLogin/resources/js/Pages/Dashboard.vue
+++ Frontend/FiturDataLogin/resources/js/Pages/Dashboard.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <!-- Flash message -->
     <div v-if="$page.props.flash.message">
       <p class="p-4 bg-green-100 text-sm">{{ $page.props.flash.message }}</p>
     </div>
